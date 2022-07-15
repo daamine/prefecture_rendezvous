add1b85740608bf6a4a73cb8755c056d03004376
Amine Daoud <daoud.amin7@gmail.com>
2022-07-15 17:25:06 +0200

    Booking RDV script from Yvelines prefecture

---
+++ README.md
@@ -0,0 +1,4 @@
+# booking RDV script
+Booking script for naturalisation RDV of Yvelines prefecture.
+When a RDV page loaded a sound notification is triggered.
+It could be used for other prefectures website with small changes.
---
+++ book.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2021 Amine Daoud
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+import logging
+from pygame import mixer
+
+URL = "https://www.yvelines.gouv.fr/booking/create/20024/0"
+logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+def refresh(browser):
+    if browser.current_url == URL:
+        logging.info("Refreshing " + browser.current_url)
+        browser.refresh()
+    else:
+        logging.info("Reload url " + URL)
+        browser.get(URL)
+
+mixer.init() #you must initialize the mixer
+s=Service(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=s)
+browser.get(URL)
+while True:
+    try:
+        time.sleep(10)
+        button = browser.find_element(By.XPATH, "//input[@type='checkbox']")
+        button.click()
+        time.sleep(5)
+        button = browser.find_element(By.XPATH, "//input[@type='submit' and @name='nextButton']")
+        button.click()
+        time.sleep(5)
+    except:
+        logging.error("error occured when trying to find the elements, retry after 1 minute")
+        time.sleep(60)
+        refresh(browser)
+        continue
+    
+    list1 = browser.find_elements(By.XPATH, "//*[contains(text(),'" + "pas calendrier disponible" + "')]")
+    list2 =  browser.find_elements(By.XPATH, "//*[contains(text(),'" + "surchargé" + "')]")
+    list3 =  browser.find_elements(By.XPATH, "//*[contains(text(),'" + "existe plus de plage horaire libre" + "')]")
+    for element in list1:
+        logging.info("Status : " + element.text)
+    for element in list2:
+        logging.info("Status : " + element.text)
+    for element in list3:
+        logging.info("Status : " + element.text)
+    if len(list1) > 0 or len(list2) > 0 or len(list3) > 0:
+        logging.warning("No RDV found, retry after 1 minute...")
+        time.sleep(60)
+        refresh(browser)
+    else:
+         break
+
+logging.info("notify !!!!!!")
+alert=mixer.Sound('mixkit-fairy-bells-583.wav')
+alert.play(loops = -1)
+time.sleep(3600) # sleep one hour
+browser.close()
